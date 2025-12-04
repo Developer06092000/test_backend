2a0fb4081a1f085e6373957e184e4be7cd84e379
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2025-12-05 01:39:35 +0500

    3

--- model.js
+++ model.js
@@ -22,7 +22,7 @@ const { Pool } = pg;
 const db = new Pool({
   connectionString:
     "postgres://postgres.wuyubslcfjbtmsylrzml:Z3SUeIc1Dpv87IS3@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x",
-  ssl: { rejectUnauthorized: false },
+  ssl: { require: true, rejectUnauthorized: false },
 });
 
 db.query(
