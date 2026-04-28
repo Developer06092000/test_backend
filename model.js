43ae0c9f873115a0b21fb0394b8b155bdcfcb641
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2026-04-29 00:07:21 +0500

    Update model.js

--- model.js
+++ model.js
@@ -26,10 +26,10 @@ if (!global.pgPool) {
     connectionString:
       // "postgres://postgres.wuyubslcfjbtmsylrzml:Z3SUeIc1Dpv87IS3@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x",
       // "postgres://postgres.wuyubslcfjbtmsylrzml:Z3SUeIc1Dpv87IS3@aws-1-us-east-1.pooler.supabase.com:6543/postgres",
-      "postgres://postgres.gxdhjnoxajfntwlbuuqv:l015gEUy8lB30zYb@aws-1-us-east-1.pooler.supabase.com:6543/postgres",
+         "postgres://postgres.gxdhjnoxajfntwlbuuqv:l015gEUy8lB30zYb@aws-1-us-east-1.pooler.supabase.com:6543/postgres",
     max: 5, // juda muhim
     idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    connectionTimeoutMillis: 5000,
   });
 }
 
